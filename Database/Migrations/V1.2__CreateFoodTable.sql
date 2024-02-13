28ab84daffb3600f660a0c00968449a5c347c7da
gajavegs <gauravsg2004@gmail.com>
2024-02-12 23:01:22 -0500

    Data import working - resolved failed int conversion

--- Database/Migrations/V1.2__CreateFoodTable.sql
+++ Database/Migrations/V1.2__CreateFoodTable.sql
@@ -1,22 +1,24 @@
 USE [Freshman15]
 GO
+
 CREATE TABLE Food (
-  [ID] INT NOT NULL PRIMARY KEY,
+  [ID] INT NOT NULL,
   [Name] VARCHAR(35) NOT NULL,
-  Calories SMALLINT NULL,
-  Carbohydrates SMALLINT NULL,
-  Protein SMALLINT NULL,
-  Fat SMALLINT NULL,
+  Calories FLOAT(24) NULL,
+  Carbohydrates FLOAT(24) NULL,
+  Protein FLOAT(24) NULL,
+  Fat FLOAT(24) NULL,
   Tier TINYINT NOT NULL,
-  ServingSize SMALLINT NULL,
+  ServingSize FLOAT(24) NULL,
   ServingUnits VARCHAR(35) NULL,
   Nutritionless BIT NULL,
   Vegetarian BIT NULL,
   Vegan BIT NULL,
   GlutenFree BIT NULL,
   ArtificialNutrition BIT NULL,
-  Meal VARCHAR(10) NULL,
-  [Day] DATE NULL
+  Meal VARCHAR(10) NOT NULL,
+  [Day] DATE NOT NULL,
+  PRIMARY KEY([ID],Meal,[Day])
 )
 
 -- Specifying schema of table for insert at allows you to only insert with some fields not null 
--- Database/Migrations/V1.3__InsertFoodTable.sql
+++ Database/Migrations/V1.3__InsertFoodTable.sql
@@ -1,9 +1,10 @@
 USE [Freshman15]
 GO
 -- Call with all the different foods, looping through the dates and meal
-CREATE PROCEDURE insertFood (@json NVARCHAR(MAX), @date Date, @meal varchar(10))
+CREATE OR ALTER PROCEDURE insertFood (@json VARCHAR(MAX), @date Date, @meal varchar(10))
 AS
 BEGIN
+BEGIN TRANSACTION
 
 --DECLARE @json NVARCHAR(MAX);
 
@@ -21,14 +22,14 @@ BEGIN
 
 DECLARE @food TABLE
 (
-  [ID] INT NOT NULL PRIMARY KEY,
+  [ID] INT NOT NULL,
   [Name] VARCHAR(35) NOT NULL,
-  Calories SMALLINT NULL,
-  Carbohydrates SMALLINT NULL,
-  Protein SMALLINT NULL,
-  Fat SMALLINT NULL,
+  Calories FLOAT(24) NULL,
+  Carbohydrates FLOAT(24) NULL,
+  Protein FLOAT(24) NULL,
+  Fat FLOAT(24) NULL,
   Tier TINYINT NOT NULL,
-  ServingSize SMALLINT NULL,
+  ServingSize FLOAT(24) NULL,
   ServingUnits VARCHAR(35) NULL,
   Nutritionless BIT NULL,
   Vegetarian BIT NULL,
@@ -38,8 +39,6 @@ DECLARE @food TABLE
   Meal VARCHAR(10) NULL,
   [Day] DATE NULL
 )
-
-UPDATE @food SET meal=@meal,[day]=@date;
 
 INSERT INTO @food (
   [ID],
@@ -59,12 +58,12 @@ INSERT INTO @food (
  SELECT * FROM OPENJSON(@json) WITH (
                         id INT 'strict $.id',
                         label VARCHAR(35) '$.label',
-                        kcal SMALLINT '$.nutrition_details.calories.value',
-                        c SMALLINT '$.nutrition_details.carbohydrateContent.value',
-                        p SMALLINT '$.nutrition_details.proteinContent.value',
-                        f SMALLINT '$.nutrition_details.fatContent.value',
+                        kcal FLOAT(24) '$.nutrition_details.calories.value',
+                        c FLOAT(24) '$.nutrition_details.carbohydrateContent.value',
+                        p FLOAT(24) '$.nutrition_details.proteinContent.value',
+                        f FLOAT(24) '$.nutrition_details.fatContent.value',
                         tier TINYINT '$.tier',
-                        ss SMALLINT '$.nutrition_details.servingSize.value',
+                        ss FLOAT(24) '$.nutrition_details.servingSize.value',
                         su VARCHAR(35) '$.nutrition_details.servingSize.unit',
                         nl BIT '$.nutritionless',
                         v BIT '$.vegetarian',
@@ -73,6 +72,30 @@ INSERT INTO @food (
                         an BIT '$.artificial_nutrition'
                         );
 
+UPDATE @food SET meal=@meal,[day]=@date;
+
 INSERT INTO Food SELECT * FROM @food; -- This will allow insertion of the read JSON into the table
+
+IF (@@ERROR = 1)
+BEGIN
+ROLLBACK TRANSACTION;
+RETURN(1);
+END
+
+
+
+--SELECT * FROM Food
+COMMIT TRANSACTION
+RETURN(0);
 END
 GO
+
+-- -- Example call
+-- EXEC insertFood @json = N'{"id":"5423187","label":"yogurt vanilla low fat","description":"string","short_name":"string","raw_cooked":1010101,"meal":"dinner","tier":2,"nutritionless":false,"artificial_nutrition":false,"nutrition":{"kcal":"60","well_being":1010101},"station_id":1010101,"station":"string","nutrition_details":{"calories":{"value":"60","unit":"string"},"servingSize":{"value":"0.3","unit":"oz"},"fatContent":{"value":"1","unit":"string"},"carbohydrateContent":{"value":"9","unit":"string"},"proteinContent":{"value":"3","unit":"string"}},"ingredients":["string[]"],"sub_station_id":1010101,"sub_station":"string","sub_station_order":1010101,"monotony":{},"vegetarian":true,"vegan":false,"glutenfree":true}'
+-- , @date = '2017-08-25',@meal = 'breakfast';
+
+-- Invalid input that appears that appears in the data ('<1')
+--  EXEC insertFood @json = N'{"id":"5423187","label":"yogurt vanilla low fat","description":"string","short_name":"string","raw_cooked":1010101,"meal":"dinner","tier":2,"nutritionless":false,"artificial_nutrition":false,"nutrition":{"kcal":"60","well_being":1010101},"station_id":1010101,"station":"string","nutrition_details":{"calories":{"value":"60","unit":"string"},"servingSize":{"value":"0.3","unit":"oz"},"fatContent":{"value":"1","unit":"string"},"carbohydrateContent":{"value":"9","unit":"string"},"proteinContent":{"value":"< 1","unit":"string"}},"ingredients":["string[]"],"sub_station_id":1010101,"sub_station":"string","sub_station_order":1010101,"monotony":{},"vegetarian":true,"vegan":false,"glutenfree":true}'
+-- , @date = '2017-08-25',@meal = 'breakfast';
+
+-- SELECT COUNT(*) FROM Food
