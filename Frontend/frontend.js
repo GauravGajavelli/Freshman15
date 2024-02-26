ba645c21ddda38fddc3e7600184772d14e2542c5
gajavegs <gauravsg2004@gmail.com>
2024-02-25 23:57:55 -0500

    Working favicon

--- Frontend/frontend.js
+++ Frontend/frontend.js
@@ -1,4 +1,9 @@
 var express = require("express");
 var app = express();
+var favicon = require('serve-favicon');
+var path = require('path');
 app.use('/static', express.static("public"));
+app.use(favicon(path.join(__dirname, 'favicon.ico')))
+
+
 app.listen(8080);
